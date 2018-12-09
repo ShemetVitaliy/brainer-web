c586fc9219af73fce3587ce2a5ef41d65a3bd63f
Shemet <vitaliy.shemet@it-devgroup.com>
2018-12-09 19:29:03 +0200

    popups

--- src/js/popup.js
+++ src/js/popup.js
@@ -10,10 +10,29 @@ $(document).ready(function() {
     $('#root').removeClass('hide');
   });
 
-  let testPopupClick = $('.bubnovskyPopup');
-  testPopupClick.click(function () {
+  let bubnovskyPopup = $('.bubnovskyPopup');
+  bubnovskyPopup.click(function () {
     $('#root').addClass('hide');
     $('.popup').removeClass('show');
     $('#bubnovskyPopup').addClass('show');
+  });
+
+  let dataMonstersPopup = $('.dataMonstersPopup');
+  dataMonstersPopup.click(function () {
+    $('#root').addClass('hide');
+    $('.popup').removeClass('show');
+    $('#dataMonstersPopup').addClass('show');
+  });
+  let greenmPopup = $('.greenmPopup');
+  greenmPopup.click(function () {
+    $('#root').addClass('hide');
+    $('.popup').removeClass('show');
+    $('#greenmPopup').addClass('show');
+  });
+  let kittelsPopup = $('.kittelsPopup');
+  kittelsPopup.click(function () {
+    $('#root').addClass('hide');
+    $('.popup').removeClass('show');
+    $('#kittelsPopup').addClass('show');
   });
 });
